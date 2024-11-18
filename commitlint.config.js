293d82d4c018eedf45b3aa739bd9a2750ed1402d
Maximo Martinez Soria <mmartinez@40q.agency>
2024-11-18 15:45:30 -0300

    fix(global): update webapp type to web in husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['api', 'webapp', 'global']],
+    'scope-enum': [2, 'always', ['api', 'web', 'global']],
   },
 };
