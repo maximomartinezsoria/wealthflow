2d2c3dfb3775ded03e5ce26eabd6e6905692318c
Maximo Martinez Soria <mmartinez@40q.agency>
2024-11-26 10:13:10 -0300

    fix(web): run eslint fix

--- wealthflow-web/src/components/ui/chart.tsx
+++ wealthflow-web/src/components/ui/chart.tsx
@@ -73,8 +73,8 @@ const ChartContainer = React.forwardRef<
 ChartContainer.displayName = "Chart"
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
-  const colorConfig = Object.entries(config).filter(
-    ([_, config]) => config.theme || config.color
+  const colorConfig = Object.values(config).filter(
+    (config) => config.theme || config.color
   )
 
   if (!colorConfig.length) {
@@ -89,7 +89,7 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
             ([theme, prefix]) => `
 ${prefix} [data-chart=${id}] {
 ${colorConfig
-  .map(([key, itemConfig]) => {
+  .map((itemConfig, key) => {
     const color =
       itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ||
       itemConfig.color
