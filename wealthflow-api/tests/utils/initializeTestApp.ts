9be854322e94dc51113ef3257cb07a665c9b9d47
Maximo Martinez Soria <mmartinez@40q.agency>
2024-12-24 12:26:11 -0300

    test(api): mock jwt guard

--- wealthflow-api/tests/utils/initializeTestApp.ts
+++ wealthflow-api/tests/utils/initializeTestApp.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppModule } from '@/app.module';
+import { JwtAuthGuard } from '@/contexts/auth/jwt-auth.guard';
 import { PrismaService } from '@/shared/infrastructure/database/prisma.service';
+
+class MockJwtAuthGuard {
+  canActivate = jest.fn().mockReturnValue(true);
+}
 
 export async function initializeTestApp() {
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
-  }).compile();
+  })
+    .overrideGuard(JwtAuthGuard)
+    .useClass(MockJwtAuthGuard)
+    .compile();
 
   const app = moduleFixture.createNestApplication();
   await app.init();
