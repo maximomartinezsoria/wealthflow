f1de73df538079084b0badce1b4d89ba10d82540
Maximo Martinez Soria <mmartinez@40q.agency>
2024-12-24 12:30:26 -0300

    test(api): user entity payday

---
+++ wealthflow-api/src/contexts/users/tests/domain/user.entity.spec.ts
@@ -0,0 +1,53 @@
+import { InvalidMonthlyIncomeException } from '@/contexts/users/domain/invalid-monthly-income.exception';
+import { InvalidPaydayException } from '@/contexts/users/domain/invalid-payday.exception';
+import { User } from '@/contexts/users/domain/user.entity';
+
+const userData = {
+  id: '1',
+  name: 'John Doe',
+  email: 'john@doe.com',
+  monthlyIncome: 1000,
+  totalMoney: 1000,
+  payday: 1,
+};
+
+describe('User Entity', () => {
+  it('should create a user', async () => {
+    const user = new User(
+      userData.id,
+      userData.email,
+      userData.name,
+      userData.monthlyIncome,
+      userData.totalMoney,
+      userData.payday,
+    );
+
+    expect(user).toBeInstanceOf(User);
+  });
+
+  it('should not create a user with negative monthly income', async () => {
+    expect(() => {
+      new User(
+        userData.id,
+        userData.email,
+        userData.name,
+        -1,
+        userData.totalMoney,
+        userData.payday,
+      );
+    }).toThrow(InvalidMonthlyIncomeException);
+  });
+
+  it('should not create a user with invalid payday', async () => {
+    expect(() => {
+      new User(
+        userData.id,
+        userData.email,
+        userData.name,
+        userData.monthlyIncome,
+        userData.totalMoney,
+        32,
+      );
+    }).toThrow(InvalidPaydayException);
+  });
+});
